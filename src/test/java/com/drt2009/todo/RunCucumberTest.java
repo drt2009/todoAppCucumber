15085ba0cb6621b8b57c8802a822afe841b3db55
Don Tobias <dtobiasalmont09@gmail.com>
2024-05-17 08:46:50 -0400

    Updated some typos

--- src/test/java/com/drt2009/todo/RunCucumberTest.java
+++ src/test/java/com/drt2009/todo/RunCucumberTest.java
@@ -3,7 +3,6 @@ package com.drt2009.todo;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
--- src/test/resources/com/drt2009/todo/todoItem.feature
+++ src/test/resources/com/drt2009/todo/todoItem.feature
@@ -1,6 +1,6 @@
 Feature: Todo Item
 
-  Scenario: Call the hello world
+  Scenario: Create a valid todo item
     Given Service is up
     When I post a new todo item with a description of "todo item"
     Then a 201 is received
