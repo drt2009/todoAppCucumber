724a1fbe61fb5ba638c6c484485342ddf1b286b3
Don Tobias <dtobiasalmont09@gmail.com>
2024-05-18 10:09:52 -0400

    Added what is needed to be able to create and get a todo item

--- src/test/java/com/drt2009/todo/StepDefinitions.java
+++ src/test/java/com/drt2009/todo/StepDefinitions.java
@@ -1,6 +1,8 @@
 package com.drt2009.todo;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
 
 import io.cucumber.java.en.*;
 
@@ -10,25 +12,51 @@ import java.util.Map;
 
 public class StepDefinitions {
 
-    private RequestSpecification request;
-    private Response response;
+  private Response response;
+  private int todoId;
 
 
   @Given("Service is up")
-    public void service_is_up() {
-    }
+  public void service_is_up() {
+  }
+  @Given("There is a todo item already created")
+  public void thereIsATodoItem() {
+    response = iPostANewTodoItemWithADescriptionOf("PremadeTestTodoItem");
+    todoId = response.getBody().jsonPath().getInt("id");
+  }
 
-    @When("I post a new todo item with a description of {string}")
-    public void iPostANewTodoItemWithADescriptionOf(String description) {
-      String endpoint = "http://localhost:8080/todo";
-        Map<String,String> body = Map.of("description", description);
-        request = given().body(body);
-        response = request.post(endpoint);
 
-    }
-    @Then("a {int} is received")
-    public void aIsReceived(Integer expectedStatusCode) {
-        response.then().statusCode(expectedStatusCode);
-    }
+  @When("I post a new todo item with a description of {string}")
+  public Response iPostANewTodoItemWithADescriptionOf(String description) {
+    String endpoint = "http://localhost:8080/todo";
+      Map<String,String> body = Map.of("description", description);
+     response = given().body(body).post(endpoint);
+     return response;
+  }
+  @When("I request the item by the id")
+  public void iRequestTheItemByTheId() {
+    String endpoint = "http://localhost:8080/todo/"+todoId;
+    response = given().post(endpoint);
+  }
+
+  @Then("a {int} is received")
+  public void aIsReceived(Integer expectedStatusCode) {
+      response.then().statusCode(expectedStatusCode);
+  }
+  @Then("the todo item is returned with an Id")
+  public void theTodoItemIsReturnedWithAnId() {
+    response.then().body("$", hasKey("id"));
+  }
+  @Then("the todo item is returned for it")
+  public void theTodoItemIsReturnedForIt() {
+    response.then().body("id", equalTo(todoId));
+  }
+
+  @Then("cleanup")
+  public void cleanup(){
+    response = null;
+    todoId = 0;
+  }
+
 
 }
--- src/test/resources/com/drt2009/todo/todoItem.feature
+++ src/test/resources/com/drt2009/todo/todoItem.feature
@@ -4,3 +4,10 @@ Feature: Todo Item
     Given Service is up
     When I post a new todo item with a description of "todo item"
     Then a 201 is received
+    And the todo item is returned with an Id
+
+  Scenario:
+    Given There is a todo item already created
+    When I request the item by the id
+    Then a 200 is recieved
+    And the todo item is returned for it
